a4e51e62ffebb5e62480dc782c71e9b0dc279d05
enyoukai <52297896+enyoukai@users.noreply.github.com>
2021-06-18 08:47:54 -0700

    more stuffs

--- YoutubeDL.plugin.js
+++ YoutubeDL.plugin.js
@@ -12,25 +12,28 @@ const childProcess = require('child_process');
 class YoutubeDL {
 
     load() { 
-        // https://stackoverflow.com/questions/21194934/how-to-create-a-directory-if-it-doesnt-exist-using-node-js
-
-        this.tmp_dir = __dirname + '\\ydl_tmp';
         this.file_pattern = /(?<=ffmpeg] Merging formats into ").+(?=")/;
         this.cache_pattern = /(?<=\[download\] ).+(?= has already been downloaded and merged)/ // TODO: get last occurrence of lookbehind
-
-        if (!fs.existsSync(this.tmp_dir))
-        {
-            fs.mkdirSync(this.tmp_dir);
-        }
-
-
     }
 
     unload() { }
     
     start() {
+        // https://stackoverflow.com/questions/21194934/how-to-create-a-directory-if-it-doesnt-exist-using-node-js
+        this.tmp_dir = __dirname + '\\ydl_tmp';
+        if (!fs.existsSync(this.tmp_dir))
+        {
+            fs.mkdirSync(this.tmp_dir);
+        }
+        
+        childProcess.exec('youtube-dl -h', (err, stdout, stderr) => {
+            if (err) BdApi.showConfirmationModal("youtube-dl not found in path", ["This plugin cannot be used if youtube-dl is not installed or not in your path"], {danger: true});
+        });
+
+        // Qwerasd's word notifications
+        this.prefix = BdApi.loadData("YoutubeDL", "prefix") ? BdApi.loadData("YoutubeDL", "prefix") : "";
         this.upload = BdApi.findModuleByProps("instantBatchUpload").upload;
-        this.sendMessagePatch = BdApi.monkeyPatch(BdApi.findModuleByProps("sendMessage"), 'sendMessage', {before: this.sendMessage.bind(this) });
+        this.sendMessagePatch = BdApi.monkeyPatch(BdApi.findModuleByProps("sendMessage"), 'sendMessage', {instead: this.sendMessage.bind(this)});
     }
 
     sendMessage(data) {
@@ -38,17 +41,14 @@ class YoutubeDL {
         const content = data.methodArguments[1].content;
         const args = content.split(' ');
         
-        if (args.length !== 2)
-            return
-        if (args[0] !== 'y-dl')
-            return
+        if (args.length !== 2 || args[0] !== this.prefix) return data.callOriginalMethod();
 
         var url = args[1];
         
-        childProcess.exec('youtube-dl --restrict-filenames -o ' + this.tmp_dir + '\\%(title)s-%(id)s.%(ext)s ' + url, (err, stdout, stderr) => {          
+        childProcess.exec('youtube-dl --restrict-filenames -o ' + this.tmp_dir + '\\%(title)s-%(id)s.%(ext)s ' + url, (err, stdout, stderr) => {
+            if (err) return BdApi.showToast(err, {type:"error"});
+   
             let output = stdout;
-            console.log(output); // uhhh debug stuff
-
             let matches = output.match(this.file_pattern);
             
             if (matches === null) matches = output.match(this.cache_pattern);
@@ -59,20 +59,37 @@ class YoutubeDL {
             
             // https://stackoverflow.com/questions/20856197/remove-non-ascii-character-in-string
             filename.replace(/[^\x00-\x7F]/g, "");
+
             fs.readFile(file_path, (err, buffer) => {
-                if (err) return console.error(err);
+                if (err) return BdApi.showToast(err, {type:"error"});
                 this.upload(channelId, new Blob([buffer]), 0, "", false, filename);
-
-            })
-
-
-            
+                
+                BdApi.showToast("Sending Video", {type:"info"});
+            }) 
         });
-        
-
     }
 
     stop() {
         this.sendMessagePatch();
     };
+
+getSettingsPanel() {
+        // Qwerasd's word notifications
+        const div = document.createElement('div');
+        const prefixT = document.createElement('h6');
+        const prefix = document.createElement('textarea');
+
+        prefixT.textContent = "Prefix"
+        prefix.placeholder = "y-dl";
+        prefix.style.width = "100%";
+        prefix.addEventListener("change", e => {
+            this.prefix = prefix.value;
+            BdApi.saveData("YoutubeDL", "prefix", this.prefix);
+        });
+
+        div.appendChild(prefixT);
+        div.appendChild(prefix);
+        
+        return div;
+    }
 }
